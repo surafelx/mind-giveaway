7445b517d515d603fc8f6363a5c636111fe4d935
luybrand <surafelyimam.sy@gmail.com>
2024-05-05 13:57:44 +0300

    1000 Likes Giveaway

--- script.js
+++ script.js
@@ -1,28 +1,62 @@
-// Replace this with your actual hidden code
-const hiddenCode = "****************";
-
 document.addEventListener("DOMContentLoaded", function () {
-  const blurredField = document.querySelector(".blurredField");
-  blurredField.innerHTML = Array.from(
-    { length: hiddenCode.length },
-    () => "*"
-  ).join("");
-});
+  const inputs = document.querySelectorAll(".safe-input");
+  const hiddenCode = document.querySelector(".hiddenCode").textContent.trim(); // Get the hidden code
+  document.querySelector(".successOutput").style.display = "none";
+  document.querySelector(".successImage").style.display = "none";
+  document.querySelector(".closeImage").style.display = "inline-block";
+  let currentIndex = 0; // Initialize the current input index
 
-document.addEventListener("DOMContentLoaded", function () {
-  const successOutputs = document.querySelectorAll(".successOutput");
-  successOutputs.forEach(function (output) {
-    output.style.display = "none";
+  // Function to move focus to the next input
+  function moveToNextInput() {
+    if (currentIndex < inputs.length - 1) {
+      inputs[currentIndex + 1].focus();
+      currentIndex++;
+    }
+  }
+
+  // Function to reset the inputs
+  function resetInputs() {
+    inputs.forEach((input) => {
+      input.value = "";
+    });
+    currentIndex = 0;
+  }
+
+  // Event listener for input changes
+  inputs.forEach((input, index) => {
+    input.addEventListener("input", function () {
+      if (input.value.length === 1) {
+        moveToNextInput();
+      }
+    });
   });
+
+  // Event listener for form submission
+  document
+    .getElementById("safeForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      // Get the entered code
+      const enteredCode = Array.from(inputs)
+        .map((input) => input.value)
+        .join("");
+
+      // Check if the entered code matches the hidden code
+      if (enteredCode === hiddenCode) {
+        alert(
+          "እንኳን ደስ አላችሁ ኮዱን ፈታቹታል! ሴፉ ውስጥ ያለውን ፅሁፍ ቶሎ ወደ ኮሜንት ልካችሁ ሽልማት ተረከቡ"
+        );
+        document.querySelector(".closeImage").style.display = "none";
+        document.querySelector(".successImage").style.display = "inline-block";
+        document.querySelector(".successOutput").style.display = "block";
+      } else {
+        // Reset the inputs
+        resetInputs();
+        // Display a message that the code doesn't match
+        alert(
+          "የፃፋቹት ኮድ ልክ አይደለም! እንደገና ይሞክሩ"
+        );
+      }
+    });
 });
-
-document
-  .getElementById("codeForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    
-  });
-
-setTimeout(() => {
-  document.getElementById("codeForm").style.display = "block";
-}, 5000); // Change 5000 to the desired time in milliseconds
